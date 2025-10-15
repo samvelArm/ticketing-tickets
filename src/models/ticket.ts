7afe4bad09bfbe0a878b4c477c917b0b28071e10
Samvel <samvel.antanyan@gmail.com>
2025-10-15 23:28:46 +0400

    Update dependencies and implement optimistic concurrency control for tickets: upgraded @samvel-ticketing/common to version 1.0.24, added mongoose-update-if-current for versioning, and created tests for concurrency control. Updated ticket model and routes to include versioning.

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   title: string;
@@ -6,7 +7,12 @@ interface TicketAttrs {
   userId: string;
 }
 
-interface TicketDoc extends mongoose.Document, TicketAttrs {}
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+  version: number;
+}
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
@@ -41,6 +47,9 @@ const ticketSchema = new mongoose.Schema(
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
